3e594bccc33309b70be27de84b75d6910bd426aa
Gerard Davis <gerardguydavis@outlook.com>
2021-08-24 15:16:48 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<h3>Addition</h3>
+<form method="get">
+First Number: <input type="number" name="first"><br>
+Second Number: <input type="number" name="second"><br>
+<button type="submit" formaction="addition.php">Add</button><br>
+<button type="submit" formaction="division.php">Divide</button><br>
+<button type="submit" formaction="subtraction.php">Subtract</button><br>
+<button type="submit" formaction="multiplication.php">Multiply</button><br>
+</form>
+<a href="index.php">Reset</a>
+</body>
+</html>
